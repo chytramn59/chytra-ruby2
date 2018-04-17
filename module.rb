3a125fa75d7faca71f16b103897e989d4ceec777
chytramn59 <cnagaraju@qwinix.io>
2018-04-17 16:06:28 +0530

    module

--- module.rb
+++ module.rb
@@ -15,60 +15,57 @@
 #         12
 #         Result is 24
 
-
-module Sample
-	puts "please select mathamatics operation"
+puts "please select mathamatics operation"
 	puts "1.addition"
 	puts "2.subtration"
     puts "3.multiplication"
     puts "4.Division"
     puts "please select option"
-     @a =gets.chomp
- def add(a)
- 		@a = 1
-		if @a == 1
+     $a =gets.to_i
+module Sample
+ def add
+ 		
+		if $a == 1
     	puts "enter value a"
-    	@b = gets.chomp
+    	@b = Integer(gets)
     	puts "enter value b"
-    	@c = gets.chomp
+    	@c = Integer(gets)
         @z = @b + @c
      puts "result:#{@z}"
-    
     	
     end
-
 end
-def subb(a)
-		@a = 2
-		if @a == 2
+def subb
+		
+		if $a == 2
     	puts "enter value a"
-    	@b = gets.chomp
+    	@b = Integer(gets)
     	puts "enter value b"
-    	@c = gets.chomp
+    	@c = Integer(gets)
     	@z = @b - @c
     	puts "sub:#{@z}"
     end
 end
-def mul(a)
-		@a = 3
-		if @a == 3
+def mul
+		
+		if $a == 3
     	puts "enter value a"
-    	@b = gets.chomp
+    	@b = Integer(gets)
     	puts "enter value b"
-    	@c = gets.chomp
+    	@c = Integer(gets)
     	@z = @b  * @c
     	puts "mul:#{@z}"
     end
 end
-def div(a)
-	@a = 4
-		if @a == 4
+def div
+	
+		if $a == 4
     	puts "enter value a"
-    	@b = gets.chomp
+    	b = Integer(gets)
     	puts "enter value b"
-    	@c = gets.chomp
-    	@z = @b * @c
-    	puts "div:#{@z}"
+    	c = Integer(gets)
+    	z = b / c
+    	puts "z"
     end
 	end
 end
@@ -78,8 +75,8 @@ class Mod
 end
 
  m = Mod.new
- m.add(1)
-  m.subb(2)
-  m.mul(3)
-  m.div(4)
+ m.add
+  m.subb
+  m.mul
+  m.div
 
